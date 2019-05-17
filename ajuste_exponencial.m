73518c1661e7f7025aa565b065680032103e7b58
Coder101BR <Coder101BR@users.noreply.github.com>
2019-05-17 20:10:42 -0300

    Add files via upload

---
+++ ajuste_exponencial.m
@@ -0,0 +1,31 @@
+% Ajuste por exponencial
+clc
+clear all
+
+x = [0.1; 1.5; 3.3; 4.5; 5];
+y = [5.9; 8.8; 12; 19.8; 21.5];
+
+n = 5;  % Porque, tem 5 pontos
+sx = 0;
+sx2 =0;
+slny = 0;
+sxlny = 0;
+
+for i =1:n
+    sx = sx + x(i);
+    sx2 = sx2 + x(i)^2;
+    slny = slny + log(y(i));
+    sxlny = sxlny + x(i)*log(y(i));
+end
+
+sx 
+sx2 
+slny
+sxlny
+
+
+a1 = (n*sxlny - sx*slny)/(n*sx2 - sx^2)
+lna0 = (slny - sx*a1)/n
+
+A1 = a1
+A0 = exp(lna0)
